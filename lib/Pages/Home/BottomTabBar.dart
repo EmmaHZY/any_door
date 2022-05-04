3329fa14aa8c25f5da72baa5f333bca8647eff01
EmmaHZY <80582278+EmmaHZY@users.noreply.github.com>
2022-05-04 18:43:16 +0800

    发布按钮

--- lib/Pages/Home/BottomTabBar.dart
+++ lib/Pages/Home/BottomTabBar.dart
@@ -3,6 +3,7 @@ import 'package:any_door/my_colors.dart';
 
 import 'package:any_door/Pages/Task/TaskPage.dart';
 import 'package:any_door/Pages/Deal/DealPage.dart';
+import 'package:any_door/Pages/Publish/PublishPage.dart';
 import 'package:any_door/Pages/Message/MessagePage.dart';
 import 'package:any_door/Pages/Mine/MinePage.dart';
 
@@ -19,7 +20,7 @@ class BottomTabBar extends StatefulWidget {
 
 class _BottomTabBarState extends State<BottomTabBar> {
   int currentIndex = 0;
-  List pageList = [const TaskPage(), const DealPage(), const MessagePage(), const MinePage()];
+  List pageList = [const TaskPage(), const DealPage(), const PublishPage(),const MessagePage(), const MinePage()];
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +29,31 @@ class _BottomTabBarState extends State<BottomTabBar> {
         title: const Text('任易门'),
       ),
       body: pageList[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
+      floatingActionButton: Container(//中心悬浮
+        height: 70,
+        width: 70,
+        padding: const EdgeInsets.all(8),
+        //设置：内边距8
+        margin: const EdgeInsets.only(top: 2),
+        //设置：外边距2
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40), color: Colors.white),
+        child: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                //可以实现重新渲染页面，因为_currentIndex变成了1，所以页面会跳转到分类页面
+                currentIndex = 2; //点击浮动按钮时，切换到分类页面
+              });
+            },
+            backgroundColor: currentIndex == 2
+                ? MyColors.mPrimaryColor
+                : Colors.grey //利用三目运算符，实现选中时，浮动按钮背景颜色变化
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      bottomNavigationBar: BottomNavigationBar(//底部导航栏布局
         currentIndex: currentIndex,
         onTap: (int index) {
           setState(() {
@@ -39,6 +64,7 @@ class _BottomTabBarState extends State<BottomTabBar> {
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.assignment_turned_in),label:"任务"),
           BottomNavigationBarItem(icon: Icon(Icons.gavel),label:"交易"),
+          BottomNavigationBarItem(icon: Icon(Icons.gavel),label:"发布"),
           BottomNavigationBarItem(icon: Icon(Icons.textsms),label:"消息"),
           BottomNavigationBarItem(icon: Icon(Icons.account_circle_rounded),label:"我的"),
         ],
---
+++ lib/Pages/Publish/PublishPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PublishPage extends StatelessWidget {
+  const PublishPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('发布'),
+    );
+  }
+}
