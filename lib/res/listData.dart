6c85394c79c90cf4cfef88b95a63eb2b8611ccfa
Palette <2945477294@qq.com>
2022-05-08 00:12:56 +0800

    new

--- lib/res/listData.dart
+++ lib/res/listData.dart
@@ -67,6 +67,108 @@ List listData = [
     "personImage":
         "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ftupian.qqjay.com%2Ftou2%2F2019%2F0919%2Fc4efab81e2da50242a58ecd665bdeea6.jpg&refer=http%3A%2F%2Ftupian.qqjay.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654512260&t=2a1a07e481d8e2b540c40a9bfb438e76",
   },
+  {
+    "taskID": "001",
+    "publisherID": "1951111",
+    "receiverID": "null",
+    "taskTitle": "求借苹果电脑充电器！！！",
+    "deadline": "2022.5.3 13:30",
+    "taskImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F6f91585a3bad622428c89dd48cf41eaf5714ff1643c40-qB6vmf_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652100417&t=f9e4b93f900f033405f5760ef20fd7a9",
+    "publishTime": "2022.5.3 11:00",
+    "taskContent": "求借苹果电脑充电器，图书馆二楼，感谢！",
+    "tag": 4,
+    "taskState": "待接受",
+    "taskCoin": 40,
+    "userName": "张某某",
+    "personImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ftupian.qqjay.com%2Ftou2%2F2019%2F0919%2Fc4efab81e2da50242a58ecd665bdeea6.jpg&refer=http%3A%2F%2Ftupian.qqjay.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654512260&t=2a1a07e481d8e2b540c40a9bfb438e76",
+  },
+  {
+    "taskID": "001",
+    "publisherID": "1951111",
+    "receiverID": "null",
+    "taskTitle": "求借苹果电脑充电器！！！",
+    "deadline": "2022.5.3 13:30",
+    "taskImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F6f91585a3bad622428c89dd48cf41eaf5714ff1643c40-qB6vmf_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652100417&t=f9e4b93f900f033405f5760ef20fd7a9",
+    "publishTime": "2022.5.3 11:00",
+    "taskContent": "求借苹果电脑充电器，图书馆二楼，感谢！",
+    "tag": 4,
+    "taskState": "待接受",
+    "taskCoin": 40,
+    "userName": "张某某",
+    "personImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ftupian.qqjay.com%2Ftou2%2F2019%2F0919%2Fc4efab81e2da50242a58ecd665bdeea6.jpg&refer=http%3A%2F%2Ftupian.qqjay.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654512260&t=2a1a07e481d8e2b540c40a9bfb438e76",
+  },
+  {
+    "taskID": "001",
+    "publisherID": "1951111",
+    "receiverID": "null",
+    "taskTitle": "求借苹果电脑充电器！！！",
+    "deadline": "2022.5.3 13:30",
+    "taskImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F6f91585a3bad622428c89dd48cf41eaf5714ff1643c40-qB6vmf_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652100417&t=f9e4b93f900f033405f5760ef20fd7a9",
+    "publishTime": "2022.5.3 11:00",
+    "taskContent": "求借苹果电脑充电器，图书馆二楼，感谢！",
+    "tag": 4,
+    "taskState": "待接受",
+    "taskCoin": 40,
+    "userName": "张某某",
+    "personImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ftupian.qqjay.com%2Ftou2%2F2019%2F0919%2Fc4efab81e2da50242a58ecd665bdeea6.jpg&refer=http%3A%2F%2Ftupian.qqjay.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654512260&t=2a1a07e481d8e2b540c40a9bfb438e76",
+  },
+  {
+    "taskID": "001",
+    "publisherID": "1951111",
+    "receiverID": "null",
+    "taskTitle": "求借苹果电脑充电器！！！",
+    "deadline": "2022.5.3 13:30",
+    "taskImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F6f91585a3bad622428c89dd48cf41eaf5714ff1643c40-qB6vmf_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652100417&t=f9e4b93f900f033405f5760ef20fd7a9",
+    "publishTime": "2022.5.3 11:00",
+    "taskContent": "求借苹果电脑充电器，图书馆二楼，感谢！",
+    "tag": 4,
+    "taskState": "待接受",
+    "taskCoin": 40,
+    "userName": "张某某",
+    "personImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ftupian.qqjay.com%2Ftou2%2F2019%2F0919%2Fc4efab81e2da50242a58ecd665bdeea6.jpg&refer=http%3A%2F%2Ftupian.qqjay.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654512260&t=2a1a07e481d8e2b540c40a9bfb438e76",
+  },
+  {
+    "taskID": "001",
+    "publisherID": "1951111",
+    "receiverID": "null",
+    "taskTitle": "求借苹果电脑充电器！！！",
+    "deadline": "2022.5.3 13:30",
+    "taskImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F6f91585a3bad622428c89dd48cf41eaf5714ff1643c40-qB6vmf_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652100417&t=f9e4b93f900f033405f5760ef20fd7a9",
+    "publishTime": "2022.5.3 11:00",
+    "taskContent": "求借苹果电脑充电器，图书馆二楼，感谢！",
+    "tag": 4,
+    "taskState": "待接受",
+    "taskCoin": 40,
+    "userName": "张某某",
+    "personImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ftupian.qqjay.com%2Ftou2%2F2019%2F0919%2Fc4efab81e2da50242a58ecd665bdeea6.jpg&refer=http%3A%2F%2Ftupian.qqjay.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654512260&t=2a1a07e481d8e2b540c40a9bfb438e76",
+  },
+  {
+    "taskID": "001",
+    "publisherID": "1951111",
+    "receiverID": "null",
+    "taskTitle": "求借苹果电脑充电器！！！",
+    "deadline": "2022.5.3 13:30",
+    "taskImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F6f91585a3bad622428c89dd48cf41eaf5714ff1643c40-qB6vmf_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652100417&t=f9e4b93f900f033405f5760ef20fd7a9",
+    "publishTime": "2022.5.3 11:00",
+    "taskContent": "求借苹果电脑充电器，图书馆二楼，感谢！",
+    "tag": 4,
+    "taskState": "待接受",
+    "taskCoin": 40,
+    "userName": "张某某",
+    "personImage":
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ftupian.qqjay.com%2Ftou2%2F2019%2F0919%2Fc4efab81e2da50242a58ecd665bdeea6.jpg&refer=http%3A%2F%2Ftupian.qqjay.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654512260&t=2a1a07e481d8e2b540c40a9bfb438e76",
+  },
 
 
 ];
